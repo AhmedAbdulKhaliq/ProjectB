d5fbe0a365526a495567fd7f67088868a1ab3fa5
AhmedAbdulKhaliq <ahmed106ak@gmail.com>
2019-04-05 23:14:22 +0500

    All done

--- ProjectB/MainForm.Designer.cs
+++ ProjectB/MainForm.Designer.cs
@@ -53,7 +53,6 @@
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.assessmentReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.manageToolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
-            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
@@ -233,8 +232,7 @@
             // assessmentReportToolStripMenuItem
             // 
             this.assessmentReportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.manageToolStripMenuItem9,
-            this.addToolStripMenuItem});
+            this.manageToolStripMenuItem9});
             this.assessmentReportToolStripMenuItem.Name = "assessmentReportToolStripMenuItem";
             this.assessmentReportToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
             this.assessmentReportToolStripMenuItem.Text = "Assessment Report";
@@ -245,13 +243,6 @@
             this.manageToolStripMenuItem9.Size = new System.Drawing.Size(152, 22);
             this.manageToolStripMenuItem9.Text = "Manage";
             this.manageToolStripMenuItem9.Click += new System.EventHandler(this.manageToolStripMenuItem9_Click);
-            // 
-            // addToolStripMenuItem
-            // 
-            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
-            this.addToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.addToolStripMenuItem.Text = "Add";
-            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
             // 
             // MainForm
             // 
@@ -303,7 +294,6 @@
         private System.Windows.Forms.ToolStripMenuItem manageToolStripMenuItem8;
         private System.Windows.Forms.ToolStripMenuItem assessmentReportToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem manageToolStripMenuItem9;
-        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
     }
 }
 
--- ProjectB/Reports.Designer.cs
+++ ProjectB/Reports.Designer.cs
@@ -29,31 +29,25 @@
         private void InitializeComponent()
         {
             this.dataReport = new System.Windows.Forms.DataGridView();
-            this.cmdgenerate = new System.Windows.Forms.Button();
             this.ObtainedMarks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmdgenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataReport)).BeginInit();
             this.SuspendLayout();
             // 
             // dataReport
             // 
             this.dataReport.AllowUserToAddRows = false;
+            this.dataReport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataReport.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataReport.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ObtainedMarks});
             this.dataReport.Location = new System.Drawing.Point(12, 104);
             this.dataReport.Name = "dataReport";
-            this.dataReport.Size = new System.Drawing.Size(413, 223);
+            this.dataReport.Size = new System.Drawing.Size(629, 223);
             this.dataReport.TabIndex = 0;
-            // 
-            // cmdgenerate
-            // 
-            this.cmdgenerate.Location = new System.Drawing.Point(130, 41);
-            this.cmdgenerate.Name = "cmdgenerate";
-            this.cmdgenerate.Size = new System.Drawing.Size(151, 36);
-            this.cmdgenerate.TabIndex = 1;
-            this.cmdgenerate.Text = "Generate Report";
-            this.cmdgenerate.UseVisualStyleBackColor = true;
-            this.cmdgenerate.Click += new System.EventHandler(this.cmdgenerate_Click);
             // 
             // ObtainedMarks
             // 
@@ -61,11 +55,34 @@
             this.ObtainedMarks.Name = "ObtainedMarks";
             this.ObtainedMarks.ToolTipText = "ObtainedMarks";
             // 
+            // cmdgenerate
+            // 
+            this.cmdgenerate.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cmdgenerate.Location = new System.Drawing.Point(236, 62);
+            this.cmdgenerate.Name = "cmdgenerate";
+            this.cmdgenerate.Size = new System.Drawing.Size(151, 36);
+            this.cmdgenerate.TabIndex = 1;
+            this.cmdgenerate.Text = "Generate Report";
+            this.cmdgenerate.UseVisualStyleBackColor = false;
+            this.cmdgenerate.Click += new System.EventHandler(this.cmdgenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CLO Report";
+            // 
             // Reports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 339);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(653, 339);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cmdgenerate);
             this.Controls.Add(this.dataReport);
             this.Name = "Reports";
@@ -73,6 +90,7 @@
             this.Load += new System.EventHandler(this.Reports_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataReport)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +99,6 @@
         private System.Windows.Forms.DataGridView dataReport;
         private System.Windows.Forms.Button cmdgenerate;
         private System.Windows.Forms.DataGridViewTextBoxColumn ObtainedMarks;
+        private System.Windows.Forms.Label label1;
     }
 }
