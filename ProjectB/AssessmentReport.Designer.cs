447e83ccb103524800f22f50d4b20d1ee0b42d16
AhmedAbdulKhaliq <ahmed106ak@gmail.com>
2019-04-05 22:56:20 +0500

    All

---
+++ ProjectB/AssessmentReport.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ProjectB
+{
+    partial class AssessmentReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdGenerateReport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ObtainedMarks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdGenerateReport
+            // 
+            this.cmdGenerateReport.Location = new System.Drawing.Point(314, 93);
+            this.cmdGenerateReport.Name = "cmdGenerateReport";
+            this.cmdGenerateReport.Size = new System.Drawing.Size(91, 28);
+            this.cmdGenerateReport.TabIndex = 1;
+            this.cmdGenerateReport.Text = "Generate Report";
+            this.cmdGenerateReport.UseVisualStyleBackColor = true;
+            this.cmdGenerateReport.Click += new System.EventHandler(this.cmdGenerateReport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Assessment Report";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(528, 150);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ObtainedMarks});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(725, 190);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ObtainedMarks
+            // 
+            this.ObtainedMarks.HeaderText = "ObtainedMarks";
+            this.ObtainedMarks.Name = "ObtainedMarks";
+            this.ObtainedMarks.ToolTipText = "ObtainedMarks";
+            // 
+            // AssessmentReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 352);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGenerateReport);
+            this.Name = "AssessmentReport";
+            this.Text = "AssessmentReport";
+            this.Load += new System.EventHandler(this.AssessmentReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cmdGenerateReport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ObtainedMarks;
+    }
+}
---
+++ ProjectB/AssessmentReport.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace ProjectB
+{
+    public partial class AssessmentReport : Form
+    {
+        public AssessmentReport()
+        {
+            InitializeComponent();
+        }
+        string constring = "Data Source = DESKTOP-P4KMVN9; Initial Catalog = ProjectB; Integrated Security = True";
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void AssessmentReport_Load(object sender, EventArgs e)
+        {
+            string query1 = "SELECT Student.RegistrationNumber, Assessment.Title, AssessmentComponent.Name As AssessmentComponent, RubricLevel.MeasurementLevel, AssessmentComponent.TotalMarks AS ComponentMarks FROM StudentResult JOIN Student ON StudentResult.StudentId = Student.Id JOIN StudentAttendance ON Student.Id = StudentAttendance.StudentId JOIN RubricLevel ON StudentResult.RubricMeasurementId = RubricLevel.Id JOIN AssessmentComponent ON StudentResult.AssessmentComponentId = AssessmentComponent.Id JOIN Assessment ON AssessmentComponent.AssessmentId = Assessment.Id";
+            SqlDataAdapter d = new SqlDataAdapter(query1, constring);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            dataGridView1.DataSource = dt;
+            int count = dataGridView1.RowCount;
+            for (int i = 0; i < count; i++)
+            {
+                double k = Convert.ToDouble(dataGridView1.Rows[i].Cells["ComponentMarks"].Value);
+                double l = Convert.ToDouble(dataGridView1.Rows[i].Cells["MeasurementLevel"].Value);
+                double marks = Convert.ToDouble((l / 4) * k);
+                dataGridView1.Rows[i].Cells[0].Value = marks;
+            }
+            //bindgrid();
+            //exportgridtopdf(dataGridView1, "Assessment Evaluation Report");
+
+        }
+        private void bindgrid()
+        {
+            SqlConnection con = new SqlConnection(constring);
+            string query1 = "SELECT Student.RegistrationNumber, Assessment.Title, AssessmentComponent.Name As AssessmentComponent, RubricLevel.MeasurementLevel, AssessmentComponent.TotalMarks AS ComponentMarks FROM StudentResult JOIN Student ON StudentResult.StudentId = Student.Id JOIN StudentAttendance ON Student.Id = StudentAttendance.StudentId JOIN RubricLevel ON StudentResult.RubricMeasurementId = RubricLevel.Id JOIN AssessmentComponent ON StudentResult.AssessmentComponentId = AssessmentComponent.Id JOIN Assessment ON AssessmentComponent.AssessmentId = Assessment.Id";
+            SqlCommand cmd1 = new SqlCommand(query1, con);
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd1;
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                BindingSource source = new BindingSource();
+                source.DataSource = dt;
+                dataGridView1.DataSource = source;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void exportgridtopdf(DataGridView d, string filename)
+        {
+            BaseFont bf = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
+            PdfPTable pdftable1 = new PdfPTable(d.Columns.Count);
+
+            pdftable1.DefaultCell.Padding = 3;
+            pdftable1.WidthPercentage = 100;
+            pdftable1.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdftable1.DefaultCell.BorderWidth = 1;
+
+            iTextSharp.text.Font text = new iTextSharp.text.Font(bf, 10, iTextSharp.text.Font.NORMAL);
+
+
+            //Header
+            foreach (DataGridViewColumn column in d.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, text));
+                cell.BackgroundColor = new iTextSharp.text.Color(240, 240, 240);
+                pdftable1.AddCell(cell);
+
+            }
+
+            // Data Row
+            foreach (DataGridViewRow row in d.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    pdftable1.AddCell(new Phrase(cell.Value.ToString(), text));
+                }
+            }
+
+            var savefiledialogue = new SaveFileDialog();
+            savefiledialogue.FileName = filename;
+            savefiledialogue.DefaultExt = ".pdf";
+            if (savefiledialogue.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream stream = new FileStream(savefiledialogue.FileName, FileMode.Create))
+                {
+                    Document pdfdoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                    PdfWriter.GetInstance(pdfdoc, stream);
+                    pdfdoc.Open();
+                    pdfdoc.Add(pdftable1);
+                    pdfdoc.Close();
+                    stream.Close();
+                    MessageBox.Show("PDF generated and saved to your PC.");
+                }
+            }
+        }
+
+        private void cmdGenerateReport_Click(object sender, EventArgs e)
+        {
+
+            
+            //bindgrid();
+            exportgridtopdf(dataGridView1, "Assessment Evaluation Report");
+            //exportgridtopdf(dataGridView1, "Report Evaluations");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
